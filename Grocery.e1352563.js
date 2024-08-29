47b91dcaf1e43db139c650a4c8693f86da644073
a1tamash <malikaltamashhh@gmail.com>
2024-08-30 03:03:15 +0530

    Updates

---
+++ Grocery.e1352563.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequiread36;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),a=()=>(0,i.jsxs)("div",{children:[(0,i.jsx)("h1",{children:"Here's your Grocery Store!"}),(0,i.jsx)("h1",{children:"Choose from a range of items..."})]})});
+//# sourceMappingURL=Grocery.e1352563.js.map
